08655987b732e66557829e35c4ee4cc19148a675
sashimizakana <atto_999@hotmail.com>
2015-04-05 22:33:19 +0900

    Create demo page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+angular.module('app',['sakana.mojier']).controller('ctrl',function(mojier){
+    var vm = this;
+    vm.search = "sushi";
+    vm.aliases = mojier.getAllAliases();
+    vm.text = "Hello! :smile::smile::smile:\nAre you hungry?\n:doughnut::custard::pizza::meat_on_bone::curry::sushi:\n";
+    vm.emoji = null;
+    vm.insertEmoji = function(emoji){
+        vm.text += ":" + emoji + ":";
+    };
+});
